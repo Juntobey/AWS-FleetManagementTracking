73c2e7dcbba77fe46d6df84f5a32a5f544c8baeb
Juntobey <teelee0811@gmail>
2026-08-23 17:32:00 +0200

    fix: add SSL support for RDS connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,11 @@
-const{Pool} = require('pg');
+const { Pool } = require('pg');
 const pool = new Pool({
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || 'HewCharaSaw@&2410',
-    database: process.env.DB_NAME || 'fleet_management'
+    database: process.env.DB_NAME || 'fleet_management',
+    ssl: process.env.DB_HOST ? { rejectUnauthorized: false } : false
 })
 
 pool.on('connect', () => {
